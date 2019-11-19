9d713b6aaa08ff21d8c24a9780ba786d67c51a7f
IceStone2735 <kyle590124@gmail.com>
2019-11-19 11:42:01 +0800

    first_step

---
+++ src/info/infoIO.java
@@ -0,0 +1,15 @@
+package info;
+
+import java.io.*;
+
+class indoIO
+{
+    public static void IO(String path)
+    {
+        File file = new File(path);
+        if (file.isFile() && file.exists())
+        {
+            
+        }
+    }
+}
