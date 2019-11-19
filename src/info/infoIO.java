3170ce4daa7b5f37fc1f8c073a6be479a766f791
IceStone2735 <kyle590124@gmail.com>
2019-11-19 12:55:36 +0800

    add_encode

--- src/info/infoIO.java
+++ src/info/infoIO.java
@@ -3,13 +3,16 @@ package info;
 import java.io.*;
 
 class indoIO
-{
+{ 
     public static void IO(String path)
     {
+        String encoding = "UTF-8";
+        int txt_long;
         File file = new File(path);
         if (file.isFile() && file.exists())
         {
-            
+            InputStreamReader read = new InputStreamReader(file, encoding);
+            txt_long = read.read();
         }
     }
 }
