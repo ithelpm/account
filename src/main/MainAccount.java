cdb422b83132b84f16af23bdf953ae760ce60743
IceStone2735 <kyle590124@gmail.com>
2019-11-22 18:57:10 +0800

    unknown_problem_1.0.2

--- src/main/MainAccount.java
+++ src/main/MainAccount.java
@@ -1,11 +1,27 @@
 package main;
 
-import info.*;
+import java.io.*;
 
 public class MainAccount
 {
     public static void main(String[] args)
     {
-        infoIO.IO("codebook.txt");
+        
+    }
+}
+
+public class infoIO {
+    public static void IO(String path)
+    {
+        String encode = "UTF-8";
+        FileReader reader = new FileReader(path);
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(reader);
+        String line = br.readLine();
+        while(line != null)
+        {
+            System.out.println(line);
+        }
+        br.close();
     }
 }
