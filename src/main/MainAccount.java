d3f52e20cd995b7e666ece873bb6422842dcfbf3
IceStone2735 <kyle590124@gmail.com>
2019-11-27 12:55:59 +0800

    finish_mainInterface

--- src/main/MainAccount.java
+++ src/main/MainAccount.java
@@ -1,16 +1,57 @@
 package main;
 
 import java.io.*;
+import java.util.*;
 
 public class MainAccount
 {
-    public static void main(String[] args) throws IOException,Error
+    public static void main(String[] args)
     {
-        infoIO.IO("C:\\Users\\user\\Desktop\\javaCrawler.txt");
+        MainInterface();
     }
-    private static void interface()
+    public static void MainInterface()
     {
-
+        MainAccount ma = new MainAccount();
+        Scanner sc = new Scanner(System.in);
+        int channel = 0;
+        while(channel!=4)
+        {
+            System.out.print("|1 = 收入");
+            System.out.println("                                                    |");
+            System.out.print("|2 = 支出");
+            System.out.println("                                                    |");
+            System.out.print("|3 = 帳目表");
+            System.out.println("                                                  |");
+            System.out.print("|4 = 離開");
+            System.out.println("                                                    |");
+            System.out.print("請輸入：");
+            channel = sc.nextInt();
+            if(channel == 1)
+            {
+                System.out.print("輸入收入：");
+                Variable.i = sc.next();
+                Variable.maximize++;
+            }
+            else if(channel == 2)
+            {
+                System.out.print("輸入收入：");
+                Variable.j = sc.next();
+                Variable.maximize++;
+            }
+            else if(channel == 3)
+            {
+                ma.Statement();
+            }
+            else if(channel >4)
+            {
+                System.out.println("錯誤!!!");
+            }
+        }
+        sc.close();
+    }
+    protected void Statement()
+    {
+        
     }
 }
 
@@ -26,4 +67,15 @@ class infoIO {
         }
         br.close();
     }
+}
+
+class Variable
+{
+    public static int maximize = 0;
+    public static String i = "0";
+    public static String j = "0";
+    public static Long income = Long.parseUnsignedLong(i);
+    public static Long expenditure = Long.parseUnsignedLong(j);
+    public static Long buffer = Long.parseUnsignedLong((Long.toString(income-expenditure)));
+    public static Long money = Long.parseUnsignedLong(Long.toString(buffer));
 }
