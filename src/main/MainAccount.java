0755956538d959ae8dfb5efbb32f4d4d01257bd9
IceStone2735 <kyle590124@gmail.com>
2019-12-04 11:23:34 +0800

    fileStatusComplete

--- src/main/MainAccount.java
+++ src/main/MainAccount.java
@@ -7,75 +7,84 @@ public class MainAccount
 {
     public static void main(String[] args)
     {
-        MainInterface();
+        Interface.MainInterface();
+        System.out.print("\033\143");
+        System.out.println();
     }
-    public static void MainInterface()
+    static class Interface
     {
-        MainAccount ma = new MainAccount();
-        Scanner sc = new Scanner(System.in);
-        int channel = 0;
-        while(channel!=4)
+        public static void MainInterface()
         {
-            System.out.print("|1 = 收入");
-            System.out.println("                                                    |");
-            System.out.print("|2 = 支出");
-            System.out.println("                                                    |");
-            System.out.print("|3 = 帳目表");
-            System.out.println("                                                  |");
-            System.out.print("|4 = 離開");
-            System.out.println("                                                    |");
-            System.out.print("請輸入：");
-            channel = sc.nextInt();
-            if(channel == 1)
+            Interface i = new Interface();
+            Scanner sc = new Scanner(System.in);
+            int channel = 0;
+            while(channel!=4)
             {
-                System.out.print("輸入收入：");
-                Variable.i = sc.next();
-                Variable.maximize++;
+                System.out.print("|1 = income");
+                System.out.println("                                                  |");
+                System.out.print("|2 = expenditure");
+                System.out.println("                                             |");
+                System.out.print("|3 = statement of account");
+                System.out.println("                                    |");
+                System.out.print("|4 = exit");
+                System.out.println("                                                    |");
+                System.out.print("input：");
+                channel = sc.nextInt();
+                System.out.flush();
+                if(channel == 1)
+                {
+                    System.out.print("input income：");
+                    Variable.i = sc.next();
+                    Variable.maximize++;
+                }
+                else if(channel == 2)
+                {
+                    System.out.print("input expenditure：");
+                    Variable.j = sc.next();
+                    Variable.maximize++;
+                }
+                else if(channel == 3)
+                {
+                    i.Statement();
+                }
+                else if(channel >4)
+                {
+                    System.out.println("ERROR!!!");
+                }
             }
-            else if(channel == 2)
-            {
-                System.out.print("輸入收入：");
-                Variable.j = sc.next();
-                Variable.maximize++;
-            }
-            else if(channel == 3)
-            {
-                ma.Statement();
-            }
-            else if(channel >4)
-            {
-                System.out.println("錯誤!!!");
-            }
+            sc.close();
         }
-        sc.close();
+        protected void Statement()
+        {
+            
+        }
     }
-    protected void Statement()
+    static class infoIO
     {
-        
+        public static void IO(String path) throws IOException
+        {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line = br.readLine();
+            System.out.println(br.readLine());
+            while((line = br.readLine() ) != null)
+            {
+                System.out.println(line);
+            }
+            br.close();
+        }
+    }
+    static class Variable
+    {
+        Variable(){}
+        public static int maximize = 0;
+        public static String i = "0";
+        public static String j = "0";
+        public static Long income = Long.parseUnsignedLong(i);
+        public static Long expenditure = Long.parseUnsignedLong(j);
+        public static Long buffer = Long.parseUnsignedLong((Long.toString(income-expenditure)));
+        public static Long money = Long.parseUnsignedLong(Long.toString(buffer));
     }
 }
 
-class infoIO {
-    public static void IO(String path) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        String line = br.readLine();
-        System.out.println(br.readLine());
-        while((line = br.readLine() ) != null)
-        {
-            System.out.println(line);
-        }
-        br.close();
-    }
-}
 
-class Variable
-{
-    public static int maximize = 0;
-    public static String i = "0";
-    public static String j = "0";
-    public static Long income = Long.parseUnsignedLong(i);
-    public static Long expenditure = Long.parseUnsignedLong(j);
-    public static Long buffer = Long.parseUnsignedLong((Long.toString(income-expenditure)));
-    public static Long money = Long.parseUnsignedLong(Long.toString(buffer));
-}
+
