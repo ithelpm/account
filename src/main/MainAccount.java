4820048558ae3d81f128e92e7fdbdbd5105772f1
IceStone2735 <kyle590124@gmail.com>
2019-12-16 20:35:58 +0800

    fix n delete some problem

--- src/main/MainAccount.java
+++ src/main/MainAccount.java
@@ -2,101 +2,90 @@ package main;
 
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
 import java.nio.charset.*;
 
 public class MainAccount
 {
     public static void main(String[] args)
     {
-        FileCreator();
+        if(Variable.myfile.exists() == false)
+        {
+	        FileCreate();
+        }
+        MainInterface();
     }
 
-    protected static void FileCreator() 
+    protected static void FileCreate()
     {
-        File file = new File(Variable.path);
-        boolean bool = true;
-        try{
-            file.createNewFile();
+        try {
+            if (Variable.myfile.createNewFile())
+                System.out.println("File created");
+            else
+                System.out.println("File already exists");
+        } catch (Exception e) {
+            System.out.println("File create failuired");
+            System.err.println(e); 
+        }
+    }
+    public static void MainInterface()
+    {
+        Scanner sc = new Scanner(System.in);
+        int channel = 0;
+        while(channel!=4)
+        {
+            System.out.print("|1 = income");
+            System.out.println("                                                  |");
+            System.out.print("|2 = expenditure");
+            System.out.println("                                             |");
+            System.out.print("|3 = statement of account");
+            System.out.println("                                    |");
+            System.out.print("|4 = exit");
+            System.out.println("                                                    |");
+            System.out.print("input：");
+            channel = sc.nextInt();
+            System.out.flush();
+            if(channel == 1)
+            {
+                System.out.print("input income：");
+                Variable.i = sc.next();
+            }
+            else if(channel == 2)
+            {
+                System.out.print("input expenditure：");
+                Variable.j = sc.next();
+            }
+            else if(channel == 3)
+            {
+                Statement(); // 這啥？寫入資料？
+            }
+            else
+            {
+                System.out.println("Error!");
+            }
+            }
+        sc.close();
+    }
+    protected static void Statement()
+    {
+        List<String> lines = Arrays.asList(Long.toString(Variable.income+Variable.expenditure));
+       try{
+            Path file = Paths.get("D:\\java_file\\AccountData.txt");
+            Files.write(file, lines, StandardCharsets.UTF_8);
         }catch(IOException e){
-            bool = false;
             e.printStackTrace();
         }
-        if (bool)
-        {
-            System.out.println("File is created!");
-        } else {
-            System.out.println("File already exists.");
-        }
-        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(Variable.path), StandardCharsets.UTF_8);
-        writer.write("Test data");
-        writer.close();
     }
-    static class Interface
-    {
-        public static void MainInterface()
-        {
-            Interface i = new Interface();
-            Scanner sc = new Scanner(System.in);
-            int channel = 0;
-            while(channel!=4)
-            {
-                System.out.print("|1 = income");
-                System.out.println("                                                  |");
-                System.out.print("|2 = expenditure");
-                System.out.println("                                             |");
-                System.out.print("|3 = statement of account");
-                System.out.println("                                    |");
-                System.out.print("|4 = exit");
-                System.out.println("                                                    |");
-                System.out.print("input：");
-                channel = sc.nextInt();
-                System.out.flush();
-                if(channel == 1)
-                {
-                    System.out.print("input income：");
-                    Variable.i = sc.next();
-                    Variable.maximize++;
-                    System.out.print("\033\143");
-                    System.out.println();
-                }
-                else if(channel == 2)
-                {
-                    System.out.print("input expenditure：");
-                    Variable.j = sc.next();
-                    Variable.maximize++;
-                    System.out.print("\033\143");
-                    System.out.println();
-                }
-                else if(channel == 3)
-                {
-                    i.Statement();
-                }
-                else if(channel >4)
-                {
-                    System.out.println("ERROR!!!");
-                    System.out.print("\033\143");
-                    System.out.println();
-                }
-            }
-            sc.close();
-        }
-        protected void Statement()
-        {
-            
-        }
-    }
-    static class Variable
-    {
-        Variable(){}
-        public static File myfile;
-        public static String fileName = "";
-        public static String path = "C:\\Windows\\java's_data.txt";
-        public static int maximize = 0;
-        public static String i = "0";
-        public static String j = "0";
-        public static Long income = Long.parseUnsignedLong(i);
-        public static Long expenditure = Long.parseUnsignedLong(j);
-        public static Long buffer = Long.parseUnsignedLong((Long.toString(income-expenditure)));
-        public static Long money = Long.parseUnsignedLong(Long.toString(buffer));
-    }
+}
+class Variable
+{
+    Variable(){}
+    public static String path = "E:\\java_file\\AccountData.txt";
+    public static File myfile = new File(path);
+    public static String i = "0";
+    public static String j = "0";
+    public static Long income = Long.parseUnsignedLong(i);
+    public static Long expenditure = Long.parseUnsignedLong(j);
+    public static Long buffer = Long.parseUnsignedLong((Long.toString(income-expenditure)));
+    public static Long money = Long.parseUnsignedLong(Long.toString(buffer));
 }
