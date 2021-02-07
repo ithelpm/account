52b97d703fd6dfd612c6da15aca4aec10f89508a
Kyle <47588731+ithelpm@users.noreply.github.com>
2021-02-07 15:15:01 +0800

    完成選擇路徑工具，但無法記憶選項

--- src/main/java/com/github/ithelpm/App.java
+++ src/main/java/com/github/ithelpm/App.java
@@ -3,23 +3,28 @@ package com.github.ithelpm;
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
+import java.util.stream.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String getDataPath = getPath();
         System.out.println(getDataPath);
     }
 
     private static String getPath() {
         try {
-            ProcessBuilder pb = new ProcessBuilder().command("python", "-u");
+            ProcessBuilder pb = new ProcessBuilder().command("python", "-u", 
+                findPath(new File(System.getProperty("user.dir")).toPath(), "readPath.py"));
             Process p = pb.start();
+
             Scanner sc = new Scanner(p.getInputStream());
             StringBuilder buffer = new StringBuilder();
+
             while (sc.hasNext()) {
                 buffer.append(sc.next());
             }
             sc.close();
+            
             return buffer.toString();
         } catch(IOException e) {
             System.err.println(e);
@@ -27,7 +32,17 @@ public class App {
         return "";
     }
 
-    private static List<Path> findPath(Path startFrom, String Name) {
-        
+    private static String findPath(Path startFrom, String Name) throws IOException {
+        String path;
+        try(
+            Stream<Path> stream = 
+                Files.find(startFrom, Integer.MAX_VALUE, (p, BasicFileAttributes)->
+                    p.getFileName().toString().equalsIgnoreCase(Name))
+        ) {
+            StringBuilder delBraces = new StringBuilder();
+            stream.collect(Collectors.toList()).forEach(delBraces::append);
+            path = delBraces.toString();
+        }
+        return path;
     }
 }
